3563e59daa2cf19d48c580e8c43c1b604288e187
sideroad <sideroad.jp@gmail.com>
2017-03-11 12:40:52 +0900

    Add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
+const logo = require('../images/logo.png');
 const styles = require('../css/header.less');
 
 const Header = ({ homeURL }) =>
@@ -8,8 +9,9 @@ const Header = ({ homeURL }) =>
     className={styles.header}
   >
     <div className={styles.logo}>
-      <Link to={homeURL} >
-        Corkpin
+      <Link to={homeURL} className={styles.link}>
+        <img src={logo} alt="corkpin" className={styles.image} />
+        <span>Corkpin</span>
       </Link>
     </div>
   </header>;
